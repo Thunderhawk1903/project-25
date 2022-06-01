8a1c3703a47bf4caac63a3fbd945fafddc266ed8
Thunderhawk1903 <81626494+Thunderhawk1903@users.noreply.github.com>
2022-06-01 14:44:10 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class paper
+{
+	constructor()
+	{
+	var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+
+	}}
+	display()
+	{
+			
+
+            image(paperImg,ball.position.x,ball.position.y,radius,radius)
+		
+			
+	}
+
+}
